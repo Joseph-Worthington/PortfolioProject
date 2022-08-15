7a62d0b77e2abd0ab3d8031d1ee814e29ca25c55
Joe Worthington <joe@authenticity.digital>
2022-08-15 22:26:12 +0100

    Desktop header, footer and hero development

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PortfolioProject
 A portfolio of all my personal work
+
+
+# Sass
+To update the sass use "sass --watch style.sass:style.css"
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,3 +5,18 @@ const mobileMenu = ()  =>{
     mobile_menu.classList.toggle('is-active');
 
 }
+
+const technologyWrapper = document.getElementsByClassName('technology-wrapper');
+const technologyCard = document.querySelector('.technology-wrapper .info-card');
+
+
+    technologyWrapper.onmouseenter = (event) => {
+        technologyCard.classList.toggle('is-active');
+        console.log('it is active');
+    };
+    
+    technologyWrapper.onmouseleave = (event) => {
+        technologyCard.classList.toggle('is-active')
+        console.log('it has been deactivated')
+    };
+
